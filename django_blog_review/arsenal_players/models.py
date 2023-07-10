d7e941d47db8eb650520ed4bb5dc21ea1a2f994e
suuu0122 <suuu0122lab@gmail.com>
2023-07-11 02:23:21 +0900

    add model and migrate

--- django_blog_review/arsenal_players/models.py
+++ django_blog_review/arsenal_players/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Player(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=100)
+    birth_date = models.DateField(blank=True)
+    introduction = models.TextField(blank=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self):
+        return self.name
