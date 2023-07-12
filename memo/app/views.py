275367adf0a479d9714ba8f3da45ffa1d3b1e567
suuu0122 <suuu0122lab@gmail.com>
2023-07-13 03:32:00 +0900

    add new templates

--- memo/app/views.py
+++ memo/app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Memo
 
 
 
 def index(request):
-    return render(request, "app/index.html")
+    memos = Memo.objects.all().order_by("-updated_datetime")
+    return render(request, "app/index.html", {"memos": memos})
+
+
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    return render(request, "app/detail.html", {"memo": memo})
+
+
+
+def new_memo(request):
+    return render(request, "app/new_memo.html")
